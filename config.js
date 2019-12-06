91d5352d2ad4709a5556cd810a9cade5233a0278
ryoctrl@gmail.com <ryoctrl@gmail.com>
2019-12-06 20:40:11 +0900

    add mobile device user agent

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const DEVICES = {
 
 const USER_AGENT = {
     [DEVICES.PC]: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-    [DEVICES.MOBILE]: '',
+    [DEVICES.MOBILE]: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/78.0.3904.84 Mobile/15E148 Safari/604.1',
 };
 
 module.exports = {
